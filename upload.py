a1d55520e7a050814bc158babb0901a089b449c4
PasoendanBrigadeZ <33935368+pasbrigadez@users.noreply.github.com>
2018-02-12 22:02:13 +0000

    Update upload.py

--- upload.py
+++ upload.py
@@ -41,6 +41,6 @@ def random_headers():
     return {'User-Agent': choice(desktop_agents),'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
 
 r = requests.post(ectitipi(), files=data, headers=random_headers())
-print r.text
+print "\33[92mResponse : \n"+r.text
 
 file.close()
