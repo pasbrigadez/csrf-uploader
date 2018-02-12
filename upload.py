c25353a358505bb4a73ea66d6d7e57539db15e74
PasoendanBrigadeZ <33935368+pasbrigadez@users.noreply.github.com>
2018-02-12 22:05:38 +0700

    Add files via upload

---
+++ upload.py
@@ -0,0 +1,43 @@
+import requests
+import sys
+from random import choice
+try:
+	url = sys.argv[1]
+	filenya = sys.argv[2]
+	postfile = sys.argv[3]
+except:
+	print "Gunakan : python upload.py <url> <pathfile> <postfile>"
+	sys.exit(1)
+
+def ectitipi():
+	if url.find("http://") > -1:
+		ectitipix = url
+		return ectitipix
+	elif url.find("https://") > -1:
+		ectitipiy = url
+		return ectitipiy
+	else:
+		ectitipiz = "http://"+url
+		return ectitipiz
+
+file = open(filenya,'r')
+
+data = {postfile:file}
+desktop_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
+ 
+def random_headers():
+    return {'User-Agent': choice(desktop_agents),'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+
+r = requests.post(ectitipi(), files=data, headers=random_headers())
+print r.text
+
+file.close()
